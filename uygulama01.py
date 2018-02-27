e895ff541a44f47a35e27e9696481ba27868c800
EbruTumer <36486999+EbruTumer@users.noreply.github.com>
2018-02-27 20:02:37 +0300

    Add files via upload

---
+++ uygulama01.py
@@ -0,0 +1,4 @@
+metin= "İnsan masum doğar ve büyüdükçe kirlenir, her ortamda farklı maskeler takmaya başlar, kendini korumak veya çıkar elde etmek için. Ama maske ile yaşanan bir hayat ancak sahte ve geçici mutluluklar getirir."
+a= len(metin)
+a=a//2
+print(metin[0:a])
